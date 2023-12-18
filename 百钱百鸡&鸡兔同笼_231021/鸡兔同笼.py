3f07d66999780b4deca93e204a4025bc8e4f7468
zhou-hack <wangyimrzhou@163.com>
2023-12-18 20:18:19 +0800

    small change

--- 百钱百鸡&鸡兔同笼_231021/鸡兔同笼.py
+++ 百钱百鸡&鸡兔同笼_231021/鸡兔同笼.py
@@ -4,15 +4,13 @@ legs = int(input("请输入腿的个数:"))
 # 判断腿总数是否奇数，腿总数不能为奇数,并且腿的个数大于0
 if (legs - heads * 2) % 2 != 0 or legs <= 0:
     print("输入数据不正确")
-    exit()
-# 计算兔子的数量
-rabbits = int((legs - heads * 2) / 2)
-chickens = int((heads - rabbits))
-
-# 判断鸡的数量是否小于0 或 兔子的数量是否小于0
-if chickens < 0 or rabbits < 0:
-    print("输入数据不正确")
-    exit()
+else:
+    # 计算兔子的数量
+    rabbits = int((legs - heads * 2) / 2)
+    chickens = int((heads - rabbits))
+    # 判断鸡的数量是否小于0 或 兔子的数量是否小于0
+    if chickens < 0 or rabbits < 0:
+        print("输入数据不正确")
 
 # 输出结果
 print('鸡有', chickens, "只", '兔有', rabbits, "只")
